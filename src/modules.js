c36b538b2f974ed5d049ca2db3a0ba02c60eda7f
Paul Trampert <paul.trampert@gmail.com>
2018-03-22 23:44:34 -0400

    Expose Th

--- src/modules.js
+++ src/modules.js
@@ -3,3 +3,4 @@ export { default as Button} from './buttons/Button.jsx';
 export { default as DropdownButton } from './buttons/DropdownButton.jsx';
 export { default as Modal} from './modals/Modal.jsx';
 export { default as Nav } from './layout/Nav.jsx';
+export { default as Th } from './list/Th.jsx';
