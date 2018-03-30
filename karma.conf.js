93925447a18b0dac624557788aa3c874e5621954
Paul Trampert <paul.trampert@gmail.com>
2018-03-30 13:02:14 +0000

    Setup tests with real browsers

--- karma.conf.js
+++ karma.conf.js
@@ -67,8 +67,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
+    browsers: ['ChromiumHeadless', 'FirefoxHeadless'],
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: [ '-headless' ]
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
